b5a6e197c20ab0742f2d1fc436991b67b4ccadd3
PC 2 <>
2024-08-16 20:28:24 -0300

    mmm

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-//poner el nombre de ka etiqueta del elemento de html antes del nombre de la funcion
-const inputOperaciones= document.querySelector("input");
+//poner el nombre de la etiqueta del elemento de html antes del nombre de la funcion
+const inputPantalla= document.querySelector("input");
+let existeOperador=false
 function actualizarVentana(boton) {
+    
     if (boton.innerHTML === "=") {
-        inputOperaciones.value= eval (inputOperaciones.value);
+        inputPantalla.value= eval (inputPantalla.value);
     } else {
-        inputOperaciones.value += boton.innerHTML;
+        
+        inputPantalla.value += boton.innerHTML;
     }
     
 }
 function borrar() {
-    inputOperaciones.value= "";
+    inputPantalla.value= "";
+}
+function borrarUltimo() {
+    inputPantalla.value=inputPantalla.value.slice(0,-1);
+    actualizarVentana();
+}
+function agregarOperador(boton) {
+    if (!existeOperador) {
+        inputPantalla
+    } if(boton.innerHTML==="+"){
+        
+    }
 }
 // eval es para que se realice la funcion aritmetica que vea dentro de los parentesis
 // innerHTML se refiere a lo q esta adentro de la etiqueta en este sentido seria lo q esta dentro de boton 

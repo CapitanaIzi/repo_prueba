d085adca6ee3c1e397c821cc7b378041799c3714
PC 2 <>
2024-08-22 19:25:15 -0300

    terminado

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 //poner el nombre de la etiqueta del elemento de html antes del nombre de la funcion
 const inputPantalla = document.querySelector("input");
-let existeOperador = false
 const OPCION_RESULTADO = "=";
 const OPCION_BORRAR_TODO = "C";
 const OPCION_BORRAR_ULTIMO = "←";
 const PANTALLA_VACIA = "";
 const OPERADORES = "+-*/%.";
-const NUMEROS = "0123456789←()";
+const NUMEROS = "0123456789←C=()";
+const OPERADOR_ESPECIAL = "√";
 
 function actualizarVentana(boton) {
     let ultimoCaracter = inputPantalla.value[inputPantalla.value.length - 1];
     if (!OPERADORES.includes(ultimoCaracter) || NUMEROS.includes(boton.innerHTML)) {
         switch (boton.innerHTML) {
             case OPCION_RESULTADO:
-                inputPantalla.value = eval(inputPantalla.value);
+
+                try {
+                    inputPantalla.value = eval(inputPantalla.value);
+                } catch (error) {
+                    alert("operacion no permitida");
+                }
                 break;
             case OPCION_BORRAR_TODO:
                 inputPantalla.value = PANTALLA_VACIA;
                 break;
             case OPCION_BORRAR_ULTIMO:
                 inputPantalla.value = inputPantalla.value.slice(0, -1);
+                break;
+            case OPERADOR_ESPECIAL:
+                inputPantalla.value = Math.sqrt(inputPantalla.value);
                 break;
             default:
                 inputPantalla.value += boton.innerHTML;
